f57e445c1d7a15ab1cd740b92f0e2fbdbef2e529
Aaron <aaron.meads@gmail.com>
2022-12-11 22:43:44 -0700

    Removing sacrament title

--- pages/sacrament/index.js
+++ pages/sacrament/index.js
@@ -4,9 +4,6 @@ const Sacrament = () => {
   return (
     <div className="hero-sacrament text-center">
       <div className="row">
-        <h1 className="text-center col-12" style={{ color: "white" }}>
-          Sacrament Meeting
-        </h1>
         <div className="hero-text">
           <div className="text-center mt-5">
             <SacramentDialog />
